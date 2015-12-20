39e7d082e4e6eb64397d2c0e2280e72e98316c9f
bryan costanich <bryancostanich@hotmail.com>
2015-12-19 21:44:17 -0800

    added fileIO stuff

--- FSharpTutorial/FileIO.fs
+++ FSharpTutorial/FileIO.fs
@@ -1,8 +1,28 @@
 ﻿module fileIO
 
 open System
+//open Microsoft.FSharp.Reflection
+open System.Reflection
+
+type Poop(poopSmell) = 
+    member this.smell = poopSmell
+
+let convertDataRow (csvLine:string) = 
+  let cells = List.ofSeq(csvLine.Split(',')) // split the csv and convert the sequence to a list
+  match cells with
+  | title::number::_ ->
+    let parsedNumber = Int32.Parse(number) 
+    (title,parsedNumber) //return a tuple of title and parsed number
+  | _ -> failwith "Incorrect data format."
+  ;;
 
 
+let enumerateResources =
+  let currentAssembly:Assembly = typeof<Poop>.GetTypeInfo().Assembly
+  let resourceNames = List.ofArray(currentAssembly.GetManifestResourceNames())
+  resourceNames
+    |> List.iter (fun r -> printfn "%A" r)
+  
 
 
 
@@ -10,5 +30,6 @@ type Exec (args) =
   member x.Run () =
     printf "test."
 
+    enumerateResources
 
     1
